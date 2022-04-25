9ebc5ea9fa749079270fb31d1264f20bd791b481
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-04-25 09:58:52 -0500

    Creación de componente Panel Usuarios
    - Se creó el componente livewire PanelUsuarios en la carpeta paneles así como su vista complementaria en la carpeta vistas-c

---
+++ app/Http/Livewire/Paneles/PanelUsuarios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Paneles;
+
+use Livewire\Component;
+
+class PanelUsuarios extends Component
+{
+    public function render()
+    {
+        return view('livewire.paneles.panel-usuarios');
+    }
+}
---
+++ resources/views/livewire/paneles/panel-usuarios.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Test Panel Usuarios</h1>
+</div>
