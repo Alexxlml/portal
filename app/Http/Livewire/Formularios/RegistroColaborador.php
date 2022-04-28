d8f18a567381bbc41b6e083ee70a7c2321208fec
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-04-27 19:29:32 -0500

    Adición de clases y variables
    - Se importó la clase Livewire\WithFileUploads
    - Se agregó use WithFileUploads para hacer uso de las funciones de guardado y url temporal
    - Se añadieron las variables necesarias para registrar un colaborador en el sistema.

--- app/Http/Livewire/Formularios/RegistroColaborador.php
+++ app/Http/Livewire/Formularios/RegistroColaborador.php
@@ -3,9 +3,19 @@
 namespace App\Http\Livewire\Formularios;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class RegistroColaborador extends Component
 {
+    // ? Uso de clase WithFileUploads para la subida de archivos
+    use WithFileUploads;
+
+    // ? Declaracion de variables
+    public $no_colaborador, $oficina, $nombre_1, $nombre_2, $ap_paterno, $ap_materno, 
+            $fecha_nacimiento, $genero, $estado_civil, $curp, $rfc, $tipo_seguro, $no_seguro,
+            $no_pasaporte, $no_visa, $domicilio, $colonia, $municipio, $estado, $nacionalidad, 
+            $codigo_postal, $paternidad, $puesto, $area, $correo, $tipo_contrato, $fecha_ingreso,$foto;
+
     public function render()
     {
         return view('livewire.formularios.registro-colaborador');
