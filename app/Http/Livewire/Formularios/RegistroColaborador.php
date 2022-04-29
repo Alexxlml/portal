ceba100dad827341cb97c7dc5187e1a277fc41ca
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-04-29 02:56:02 -0500

    Corrección de Funciones escuchadoras
    - Se eliminaron las funciones que escuchaban las variables de los radio buttons.
    
    - Se eliminaron las variables que servían para comprobar los valores de los radio buttons

--- app/Http/Livewire/Formularios/RegistroColaborador.php
+++ app/Http/Livewire/Formularios/RegistroColaborador.php
@@ -25,18 +25,6 @@ class RegistroColaborador extends Component
         $no_pasaporte = '', $no_visa = '', $domicilio, $colonia, $municipio, $estado, $nacionalidad,
         $codigo_postal, $paternidad, $puesto, $area, $correo, $tipo_contrato, $fecha_ingreso, $foto, $foto_ruta;
 
-    // ? Variables de radio button
-    // * Genero
-    public $genero_masculino, $genero_femenino, $genero_no_binario;
-    // * Estado Civil
-    public $estado_civil_soltero, $estado_civil_casado, $estado_civil_union_libre;
-    // * Paternidad
-    public $paternidad_si, $paternidad_no;
-    // * Tipo Seguro
-    public $tipo_seguro_sgm, $tipo_seguro_imss;
-    // * Tipo Contrato
-    public $tipo_contrato_honorarios;
-
     // ? Inicio Alertas
     public function getListeners()
     {
@@ -182,62 +170,6 @@ class RegistroColaborador extends Component
             ]);
         }
     }
-
-    // ? Funciones que escuchan los cambios en las variables de radio button y asignan el resultado en variables generales
-    // * Genero
-    public function updatedGeneroMasculino()
-    {
-        $this->genero_masculino ? $this->genero = 1 : "";
-    }
-    public function updatedGeneroFemenino()
-    {
-        $this->genero_femenino ? $this->genero = 2 : "";
-    }
-    public function updatedGeneroNoBinario()
-    {
-        $this->genero_no_binario ? $this->genero = 3 : "";
-    }
-
-    // * Estado Civil
-    public function updatedEstadoCivilSoltero()
-    {
-        $this->estado_civil_soltero ? $this->estado_civil = 1 : "";
-    }
-    public function updatedEstadoCivilCasado()
-    {
-        $this->estado_civil_casado ? $this->estado_civil = 2 : "";
-    }
-    public function updatedEstadoCivilUnionLibre()
-    {
-        $this->estado_civil_union_libre ? $this->estado_civil = 3 : "";
-    }
-
-    // * Paternidad
-    public function updatedPaternidadSi()
-    {
-        $this->paternidad_si ? $this->paternidad = 1 : "";
-    }
-    public function updatedPaternidadNo()
-    {
-        $this->paternidad_no ? $this->paternidad = 0 : "";
-    }
-
-    // * Tipo Seguro
-    public function updatedTipoSeguroSgm()
-    {
-        $this->tipo_seguro_sgm ? $this->tipo_seguro = 1 : "";
-    }
-    public function updatedTipoSeguroImss()
-    {
-        $this->tipo_seguro_imss ? $this->tipo_seguro = 2 : "";
-    }
-
-    // * Tipo Contrato
-    public function updatedTipoContratoHonorarios()
-    {
-        $this->tipo_contrato_honorarios ? $this->tipo_contrato = 1 : "";
-    }
-
 
     // ? Generador de numero de colaborador de acuerdo al ultimo registro de la base de datos
     public function generadorNoColaborador()
