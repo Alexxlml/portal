90d6f150a310d5e32308d99a21213cf6ccb0224e
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-05-04 16:02:39 -0500

    Creación de componente, vista y vista complementaria para panel facturas

---
+++ app/Http/Livewire/Paneles/PanelFacturas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Paneles;
+
+use Livewire\Component;
+
+class PanelFacturas extends Component
+{
+    public function render()
+    {
+        return view('livewire.paneles.panel-facturas');
+    }
+}
---
+++ resources/views/livewire/paneles/panel-facturas.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Test Facturas</h1>
+</div>
