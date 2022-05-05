6f8a2d82d7c5973f21bd58f5222647c30c23e70c
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-05-05 11:46:00 -0500

    Creación de Componente PanelFacturasUsuarios y vistas

---
+++ app/Http/Livewire/Paneles/PanelFacturasUsuarios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Paneles;
+
+use Livewire\Component;
+
+class PanelFacturasUsuarios extends Component
+{
+    public function render()
+    {
+        return view('livewire.paneles.panel-facturas-usuarios');
+    }
+}
---
+++ resources/views/livewire/paneles/panel-facturas-usuarios.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Test facturas usuarios</h1>
+</div>
