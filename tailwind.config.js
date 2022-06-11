2bca7a022ef5fc470fc949389b97f74db0ee0fbe
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-06-11 12:22:47 -0500

    Nuevas variables de color
    - Se agregaron los colores del brandbook de silcon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,30 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
+        },
+        colors: {
+            "verde-silconio": "#30b472",
+            "verde-tecnologico": "#7fc57f",
+            "negro-inicio": "#221f1f",
+            "negro-azulino": "#262c38",
+            "blanco-hueso": "#e6e6e5",
+            "celeste-guia": "#7dcede",
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
