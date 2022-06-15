e8c9bb91de92c02a7263becb625fd0081e06432b
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-06-15 11:35:02 -0500

    Modificaciones en Favicon
    - Se modificó el favicon agregandolo a la carpeta images y actualizando su referencia en los layouts app y guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ URL::asset('images/favicon.ico') }}" type="image/x-icon" />
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
