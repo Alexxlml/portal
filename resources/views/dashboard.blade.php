2c2f216a83ac3e67c61e6565a93a27a097c6d1c7
Marco Zacarias <azacarias@silcon.tech>
2023-02-15 12:52:43 -0600

    Nuevo Dashboard
    - Se agregó un nuevo dashboard para recibir a los colaboradores en el Portal.
    
    - Se agregaron recursos a app.css y tailwind.config.js para reproducir la animación del nuevo dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    <div class="bg-gray-50 h-[93vh] flex items-center justify-center px-16">
+        <div class="relative w-full max-w-lg">
+            <div class="absolute top-0 -left-4 w-72 h-72 bg-verde-silconio-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div class="absolute top-0 -right-4 w-72 h-72 bg-negro-inicio-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div class="absolute -bottom-8 left-20 w-72 h-72 bg-celeste-guia-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div class="m-8 relative space-y-4">
+                <div class="bg-blanco-hueso-50/[.50] border-2 border-verde-silconio-500 rounded-lg p-8">
+                    <h1 class="text-3xl font-bold text-center">Bienvenido a Portal</h1>
+                    <p class="text-xl mt-2 text-center">Utiliza el menú superior <span class="font-bold italic">Factura</span> para comenzar</p>
+                </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
